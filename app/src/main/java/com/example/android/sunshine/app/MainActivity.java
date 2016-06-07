3554421f190d6130f72060e6d192cd565c208b1d
Cagrosso <cameron.panagrosso+git@gmail.com>
2016-06-07 18:09:00 -0400

    Finish Lesson 2
    Lesson 2 finished, not continuing to update app/build.gradle as that
    file contains sensitive API key

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine.app;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,10 +42,15 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast toast = Toast.makeText(context, "Settings Pressed", duration);
+            toast.show();
             return true;
         }
 
